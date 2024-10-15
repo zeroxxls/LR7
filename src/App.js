36bac9c05c50e0315e410d17a8343b5e6176202f
zeroxxl <zeroxxhls@gmail.com>
2024-10-15 15:23:58 +0200

    final commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React, { useState } from "react";
+// App.js
+import React, { useEffect, useState } from "react";
 import List from "./List";
 import Alert from "./Alert";
+const getLocalStorage = () => {
+    let list = localStorage.getItem("list");
+    if (list) {
+      return JSON.parse(list);
+    } else {
+      return [];
+    }
+  };
 
 function App() {
   const [name, setName] = useState("");
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(getLocalStorage());
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
-  
+  const [alert, setAlert] = useState({ show: false, type: "", msg: "" });
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name) {
+      showAlert(true, "danger", "please enter values");
     } else if (name && isEditing) {
+      const editItem = list.map((item)=>{
+        if(item.id === editID){
+          item.title =name;
+        }
+        return item
+      });
+      setList(editItem);
+      setEditID(null);
+      setIsEditing(false);
+      showAlert(true,"success", "item edited")
     } else {
+      showAlert(true, "success", "task added to the List");
       const newItem = {
         id: new Date().getTime().toString(),
         title: name,
@@ -22,14 +44,40 @@ function App() {
     }
   };
 
-  const clearItems = () => {
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+    if (show) {
+      setTimeout(() => {
+        setAlert({ show: false, type: "", msg: "" });
+      }, 3000); // Закриття alert через 3 секунди
+    }
+  };
+
+  const clearList = () => {
+    showAlert(true, "danger", "empty list");
     setList([]);
+  };
+  useEffect(()=>{
+    localStorage.setItem("list",JSON.stringify(list));
+  }, [list])
+
+  const removeItem = (id) => {
+    const filteredList = list.filter((task) => task.id !== id);
+    setList(filteredList);
+    showAlert(true, "danger", "item removed");
+  };
+
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
   };
 
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-      {alert.show && <Alert {...alert} />}
+        {alert.show && <Alert {...alert} />}
         <h3>TODO list</h3>
         <div className="form-control">
           <input
@@ -46,8 +94,8 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} />
-          <button className="clear-btn" onClick={clearItems}>
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button className="clear-btn" onClick={clearList}>
             clear items
           </button>
         </div>
--- src/List.js
+++ src/List.js
@@ -1,26 +1,36 @@
+// List.js
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = ({items}) => {
-  return(
+
+const List = ({ items, editItem, removeItem }) => {
+  return (
     <div className='grocery-list'>
-      {items.map((item)=>{
-        const {id,title} = item;
-        return(
-          <article key={id} className='grocery-list'>
+      {items.map((item) => {
+        const { id, title } = item;
+        return (
+          <article key={id} className='grocery-item'>
             <p className='title'>{title}</p>
             <div className='btn-container'>
-              <button type='button' className='FaEdit'>
-                <FaEdit/>
+              <button
+                type='button'
+                className='edit-btn'
+                onClick={() => editItem(id)}
+              >
+                <FaEdit />
               </button>
-              <button type='button' className='FaTrash'>
-                <FaTrash/>
+              <button
+                type='button'
+                className='delete-btn'
+                onClick={() => removeItem(id)}
+              >
+                <FaTrash />
               </button>
             </div>
           </article>
-        )
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
